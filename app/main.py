bb2575ca76d36f376b213d0083f50707c9712ff5
sanjay11-8 <sanjaykumar111008@gmail.com>
2025-08-01 21:33:09 +0530

    Fixed module import for Streamlit deployment

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from prediction_helper import predict  # Ensure this is correctly linked to your prediction_helper.py
+from app.prediction_helper import predict
+  # Ensure this is correctly linked to your prediction_helper.py
 
 # Set the page configuration and title
 st.set_page_config(page_title="Lauki Finance: Credit Risk Modelling", page_icon="📊")
